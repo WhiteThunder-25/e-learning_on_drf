3e3dbc0817353faccc128e96f1f9cc15826cac33
Marina <lav_mari@mail.ru>
2025-07-29 05:18:54 +0300

    3

---
+++ lms/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from lms import views
+from lms.apps import LmsConfig
+from lms.views import (CourseViewSet, LessonCreateView, LessonDeleteView, LessonDetailView, LessonListView,
+                       LessonUpdateView, SubscriptionAPIView)
+
+
+app_name = LmsConfig.name
+
+router = DefaultRouter()
+router.register(r'courses', views.CourseViewSet, basename='courses')
+urlpatterns = [
+    path("lessons/create/", LessonCreateView.as_view(), name="create_lesson"),
+    path("lessons/", LessonListView.as_view(), name="lessons"),
+    path("lessons/<int:pk>/", LessonDetailView.as_view(), name="lesson"),
+    path("lessons/<int:pk>/update/", LessonUpdateView.as_view(), name="update_lesson"),
+    path("lessons/<int:pk>/delete/", LessonDeleteView.as_view(), name="delete_lesson"),
+    path("subscription/<int:pk>/", SubscriptionAPIView.as_view(), name="subscription"),
+    path('', include(router.urls)),
+]
